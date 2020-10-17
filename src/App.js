ff1ea5aca84df1c2a93cbd655270a772b7bd31da
cdsalty <soltiscd@gmail.com>
2020-10-17 16:09:54 -0400

    still working with it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchForm from './components/SearchForm';
 import CardList from './components/CardList';
+
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
 
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState({});
+
+  useEffect(() => {
+    fetchFormData();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchFormData = async () => {
+    try {
+      const url = `https://api.pokemontcg.io/v1/cards?pageSize=50`; // max 50 results
+      const { data } = await axios.get(url);
+      // we need to call setCards and pass in the data we're after
+      // setCards(data);
+      setCards(data.cards);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   return (
     <div>
@@ -14,8 +34,9 @@ const App = () => {
           <h1 className="text-3xl text-white py-2">Pokemon Search</h1>
         </div>
         <div className="flex flex-col">
-          <SearchForm setCards={setCards} />
-          <CardList cards={cards} />
+          <SearchForm />
+          <CardList data={cards} />
+
         </div>
       </h1>
     </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,24 +1,7 @@
-import React, { useEffect } from 'react'
-import axios from 'axios';
+import React from 'react'
 
 
-const SearchForm = ({ setCards }) => {
-  useEffect(() => {
-    fetchFormData();
-    // eslint-disable-next-line
-  }, []);
-
-  const fetchFormData = async () => {
-    try {
-      const url = `https://api.pokemontcg.io/v1/cards?pageSize=50`; // max 50 results
-      const { data } = await axios.get(url);
-      // we need to call setCards and pass in the data we're after
-      setCards(data);
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
+const SearchForm = () => {
 
   return (
     <div>
@@ -28,3 +11,35 @@ const SearchForm = ({ setCards }) => {
 }
 
 export default SearchForm;
+
+
+// import React, { useEffect } from 'react'
+// import axios from 'axios';
+
+
+// const SearchForm = ({ setCards }) => {
+//   useEffect(() => {
+//     fetchFormData();
+//     // eslint-disable-next-line
+//   }, []);
+
+//   const fetchFormData = async () => {
+//     try {
+//       const url = `https://api.pokemontcg.io/v1/cards?pageSize=50`; // max 50 results
+//       const { data } = await axios.get(url);
+//       // we need to call setCards and pass in the data we're after
+//       setCards(data);
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+
+//   return (
+//     <div>
+//       <p className="text-green-600">SearchForm</p>
+//     </div>
+//   )
+// }
+
+// export default SearchForm;
